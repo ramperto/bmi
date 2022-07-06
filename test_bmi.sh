c6b16f1e7cdc8b443fc562329632bb07709ff5a8
rampertomember <ramperto@member.id>
2022-07-06 16:59:04 +0700

    update test

--- test_bmi.sh
+++ test_bmi.sh
@@ -1,5 +1,5 @@
 sleep 5
-if curl api:3000/?height=174&weight=88 | grep -q 'Overweight'; then
+if curl "api:3000/?height=174&weight=88" | grep -q 'Overweight'; then
   echo "Tests passed!"
   exit 0
 else
